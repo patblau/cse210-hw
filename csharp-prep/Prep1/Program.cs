b0dc26e390636a8703c9cccb8cc7398c134873c4
Pat Blau <that_email_here>
2026-02-24 04:08:44 -0600

    changed greeting and created question 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Hello Prep1 World!");
         Console.WriteLine("Hay there, hellow");
 
         Console.WriteLine("What is your first name? ");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Howdy there.");
+
+        Console.WriteLine("What is your grade persentage? ");
+        string gradePersentage = Console.ReadLine();
+        
+        
+       
     }
-}
+    }
