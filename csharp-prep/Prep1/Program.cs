8e8c4795acdf4af9e23a837b8f5acf1d1ca57150
Pat Blau <that_email_here>
2026-02-24 03:08:50 -0600

    add greeting

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hay there, hellow");
+        
+       
     }
 }
