f641ea673299080b15f7c5b2c4070862b91e01e3
Pat Blau <that_email_here>
2026-02-24 11:49:59 -0600

    set up grade question

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,13 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
-
         Console.WriteLine("Howdy there.");
 
         Console.WriteLine("What is your grade persentage? ");
         string gradePersentage = Console.ReadLine();
+        int percentage = int.Parse(gradePersentage);
+        string grade = "";
         
         
-       
     }
-    }
+}
