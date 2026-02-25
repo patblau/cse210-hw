9d8e697842d030d31dc5ea700b6e0ae3bbd31564
Pat Blau <that_email_here>
2026-02-25 17:11:52 -0600

    added int guess code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -11,11 +11,13 @@ class Program
         //invite to play and retrieve a number
         Console.WriteLine("Lets play the magig number game, shall we?");
         Console.WriteLine("I will pick a number and you can guess what it is.");
+        
         Console.Write("Retrieve a magical number: ");
         int mgcNmbr = int.Parse(Console.ReadLine());
 
-       //have them quess
+       //have them quess a number
        Console.Write("Pick your number: ");
+       int guess = int.Parse(Console.ReadLine());
 
         //respond higher and lower
         if (guess < mgcNmbr)
@@ -24,7 +26,5 @@ class Program
             Console.WriteLine("Lower");
         else
             Console.WriteLine("Wow! You Guessed it!");
-
-        
     }
 }
