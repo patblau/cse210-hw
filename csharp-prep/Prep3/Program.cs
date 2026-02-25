14b2c5cb9ae9476f5d29d31fdf9acf98dd0d5d86
Pat Blau <that_email_here>
2026-02-25 16:32:20 -0600

    set up greeting and code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Ciao Prep3 World!");
+
+        //invite to play and retrieve a number
+        Console.WriteLine("Lets play the magig number game, shall we?");
+        Console.WriteLine("I will pick a number and you can guess what it is.");
+        Console.Write("Retrieve a magical number: ");
+
+        int mgcNmbr = int.Parse(Console.ReadLine());
+
+        //respond higher and lower
+        if (guess < mgcNmbr)
+            Console.WriteLine("Higher");
+        else if (guess > mgcNmbr)
+            Console.WriteLine("Lower");
+        else
+            Console.WriteLine("Wow! You Guessed it!");
+
+        
     }
 }
