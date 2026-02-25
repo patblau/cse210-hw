cc11f2bba8ce58eb335006cdea8012b1b9d05dd4
Pat Blau <that_email_here>
2026-02-25 10:02:18 -0600

    set up for modulas operator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,11 +7,14 @@ class Program
         Console.WriteLine("Hello Prep2 World!");
         Console.WriteLine("Howdy there.");
 
+        //ask for the grade percentage
         Console.WriteLine("What is your grade percentage? ");
         string gradePercentage = Console.ReadLine();
         int percentage = int.Parse(gradePercentage);
         string grade = "";
         
+       //define grades using separate print statements
+       //change veribles into letter grades
         if(percentage >= 90)
             grade = "A";
         else if(percentage >= 80)
@@ -22,10 +25,17 @@ class Program
             grade = "D";
         else
             grade = "F";
+
+        if (grade )
+
+        //include "+ (>=7)" or "- (<3)" to the grade
+        // use modulus operator %
+        int lastDigit = percentage % 10;
         
         Console.WriteLine($"\nYour grade is {grade}");  
 
-        if (percentage >= 70)
+        //Need 70% to pass, state if passed or not
+        if (percentage >= 70) 
         {
             Console.WriteLine("You did it. You passed the class. Congradulations!");
         }
