92a883fb7cbcb2af4626a2a3fac9184c9261e263
Pat Blau <that_email_here>
2026-02-26 00:37:33 -0600

    codes for sum on numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,16 +8,34 @@ class Program
     {
         Console.WriteLine("Olá! Prep4 World!");
         
-        List<int> numbers = new List<int>();	
+        List<int> nmbrs = new List<int>();	
 
-		//Ask the user for a series of numbers, and 	
+		//Ask the user for a series of numbers	
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");						
-        int userNumber = -1;				
         
-        // append each number to a list. 
+        // append each number to a list. Stop when they enter 0.
+        int usrNmbr = -1;
+        while (unNmbr !-0)
+        {
+            Console.Write("Enter number: ");	
+            usrNmbr = int.Parse(Console.ReadLine());	
+             
+            if (usrNmbr != 0)
+            {	 	
+                nmbrs.Add(usrNmbr);	
+            }					
 
-        // Stop when they enter 0.
+        // sum of list
+        int sum = 0;
+        foreach (int number in nmbrs)
+            {
+                sum += number;
+            }
+        Console.WriteLine($"The sum is: {sum}");
+        
 
-
+        
+        // average of list
+        // maximum of list
     }
 }
