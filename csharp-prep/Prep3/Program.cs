aaf54f79ab4790f6637f8b6ec7b91c0ba86bd1bb
Pat Blau <that_email_here>
2026-02-25 19:11:32 -0600

    moved changes to program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,34 +3,37 @@ using System.Linq.Expressions;
 using System.Reflection;								
 using System.Runtime.InteropServices;								
 								
-class Program								
+class Program															
 {								
 	static void Main(string[] args)							
 	{							
-		Console.WriteLine("Ciao Prep3 World!");						
+		Console.WriteLine("Ciao Prep3 World!");	
+
+        //computer picks random number
+        Random randomGenerator = new Random();
+        int mgcNumbr = randomGenerator.Next(1, 101);					
 								
-		//invite to play and retrieve a number						
+		//invite to play and retrieve a number 1/100					
 		Console.WriteLine("Lets play the magig number game, shall we?");						
 		Console.WriteLine("I will pick a number and you can guess what it is.");						
-		Console.Write("Retrieve a magical number: ");						
-		int mgcNmbr = int.Parse(Console.ReadLine());						
+		Console.WriteLine("The number will be between 1 and 100.");											
 								
-		//have them quess a number						
-		Console.Write("Pick your number: ");						
+		//quess a number						
+		Console.Write("Pick a number: ");						
 		int guess = int.Parse(Console.ReadLine());						
 								
 		//loop with responce of higher and lower						
 		while (guess != mgcNumbr)
         {
             if (guess < mgcNmbr)						
-			    Console.WriteLine("Higher");					
+			    Console.WriteLine("Go higher");					
 		    else 						
-			    Console.WriteLine("Lower");					
-		    else						
-			    Console.WriteLine("Wow! You Guessed it!");					
+			    Console.WriteLine("Go lower");					
+		   						
+		Console.WriteLine("Wow! You Guessed it!");					
 	    }
 
         //responce for wrong answer
-        Console.Write("Woops, no. Try again: ");							
+        Console.WriteLine("Woops, nope. Try again: ");							
     }
-}								
+}	
