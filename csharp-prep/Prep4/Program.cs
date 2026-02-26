c4c63a053f606bd7f5640f0bb26b18be5e095fab
Pat Blau <that_email_here>
2026-02-26 00:12:24 -0600

    code for list of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Olá! Prep4 World!");
+        
+        List<int> numbers = new List<int>();	
+
+		//Ask the user for a series of numbers, and 	
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");						
+        int userNumber = -1;				
+        
+        // append each number to a list. 
+
+        // Stop when they enter 0.
+
+
     }
 }
