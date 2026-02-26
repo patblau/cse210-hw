44f12cd543d06f5bc115216304b0a7942ebca638
Pat Blau <that_email_here>
2026-02-25 18:55:54 -0600

    removed extra guess request

--- csharp-prep/Prep3/Program3b.cs
+++ csharp-prep/Prep3/Program3b.cs
@@ -16,8 +16,25 @@ class Program
 		//invite to play and retrieve a number 1/100					
 		Console.WriteLine("Lets play the magig number game, shall we?");						
 		Console.WriteLine("I will pick a number and you can guess what it is.");						
-		Console.Write("To play retrieve a magical number: ");						
-		int mgcNmbr = int.Parse(Console.ReadLine());						
+		using System;
+using System.Linq.Expressions;
+using System.Reflection;								
+using System.Runtime.InteropServices;								
+								
+class Program								
+{								
+	static void Main(string[] args)							
+	{							
+		Console.WriteLine("Ciao Prep3 World!");	
+
+        //computer picks random number
+        Random randomGenerator = new Random();
+        int mgcNumbr = randomGenerator.Next(1, 101);					
+								
+		//invite to play and retrieve a number 1/100					
+		Console.WriteLine("Lets play the magig number game, shall we?");						
+		Console.WriteLine("I will pick a number and you can guess what it is.");						
+		Console.WriteLine("The number will be between 1 and 100.");											
 								
 		//have them quess a number						
 		Console.Write("Take a guess: ");						
@@ -37,4 +54,4 @@ class Program
         //responce for wrong answer
         Console.Write("Woops, no. Try again: ");							
     }
-}								
+}		
