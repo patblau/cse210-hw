78b0fa08c5043493f47d4b40ca745f7c369ad2d8
Pat Blau <that_email_here>
2026-02-26 19:05:38 -0600

    code to ask for name

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -15,6 +15,10 @@ class Program
         Console.WriteLine("Welcome to the Program!");
     }
 
-       
+    // Asks for and returns the user's name (string)
+    static string PromptUserName()
+    {
+        Console.Write("Emter your name here: ");
     }
+       
 }
