d6a2890642ac66b3ce08a3b566d9d05810c191b9
Pat Blau <that_email_here>
2026-03-01 17:14:46 -0600

    Setting up Lesson2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Bonjour! Learning02 World!");
+
+        //person's name and list of jobs
+        class Resume
+
+
+        //jobs: Company, Job title, Start Year, End Year
+        class Job
     }
+    
 }
