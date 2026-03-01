0b09b80bce361dea40ebb041762a0c6ae4da7506
Pat Blau <that_email_here>
2026-03-01 17:24:02 -0600

    added dissplay method

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -14,7 +14,8 @@ class Program
             public string _jobTitle;
             public string _startYear;
             public string _endYear;
-
+            
+            public void Display()
         }
 }
 
