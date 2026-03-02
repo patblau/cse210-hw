3ad17f1e4016b2fe665928e0ba3d8a3d1f3180be
Pat Blau <that_email_here>
2026-03-01 18:13:30 -0600

    changed lines 39 & 40 to "int"

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -36,8 +36,8 @@ internal class NewBaseType
         {
             public string _company;
             public string _jobTitle;
-            public string _startYear;
-            public string _endYear;
+            public int _startYear;
+            public int _endYear;
             public void Display()
             {
             Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
