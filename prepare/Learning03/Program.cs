ab6a647fdc4aa1fad47ba2d1441645e6cb1a986c
Pat Blau <that_email_here>
2026-03-02 21:46:46 -0600

    Journal: menu loop and class scaffolding

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,11 +1,60 @@
 using System;
+using System.Diagnostics;
+using System.Xml.Serialization;
+using System.IO;
 
-class Program
+StreamWriter class
+{ 
+    string Journal = "myJournal.text";
+}
+    outputFile.WriteLine()
+
+class Program 
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Namaste! Learning03 World!");
+        Console.WriteLine("Welcome to Your Journal Assistant.");
 
+        // Step 1 Write a new entry 
+        
+        // Display prompt from list 
+       
+        {
+            
+        }
+        // Show random prompt 
+        string prompt = promptGen.GetRandomPrompt();
+        Console.WriteLine(prompt);
+
+
+        // Get today's date
+        public Entry(string prompt, string response)
+        {
+            Date = DateTime.Now.ToString("yyyy-MM-dd");
+            Prompt = prompt;
+            Response = response;
+        }
+        // User selects "Write"
+        // Add Entry to Journal and save
+        // PrompGenerator returns random prompt
+        
+        // Step 2 Display the Journal
+        // Iterate through all entries in the journal 
+        // Display on screen
+
+        //Step 3 Save the fournal file
+        // ask for a file name and save
+
+        //Step 4 Loan the journal
+        //Prompt for filename
+        // load to journal
+
+        //Step 5 Provide a minue to pick from choices
+
+        // Step 6 list of 5 different prompts
+
+        //Step 7 interface to follow the video.
 
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Namaste! Learning03 World!");
+        Console.WriteLine("Welcome to Your Journal Assistant.");
+        Console.ReadLine();
+
+        Journal journal = new Journal();
+        PropmtGenerator promptGen = new PropmtGenerator():
+
+        //Step 5 Provide a minue to pick from choice
+        //Step 7 interface as intructed by video
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("To start, select one of following:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do? ");
+
+            string choice = ConsoleReadLine();
+            ConsoleWriteLine();
+
+           
+        }
+
+    }
+        // Step 1 Display prompt from list
+        // Show random prompt
+        // Get today's date
+        // User selects "Write"
+        // Add Entry to Journal and save
+        // PrompGenerator returns random promp  
+        // Step 2 Display the Journal
+        // Iterate through all entries in the journal 
+        // Display on scree
+        //Step 3 Save the fournal file
+        // ask for a file name and sav
+        //Step 4 Loan the journal
+        //Prompt for filename
+        // load to journa
+        //Step 5 Provide a minue to pick from choice
+        // Step 6 list of 5 different prompts
+
+        //Step 7 interface to follow the video.
     }
 }
