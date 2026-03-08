d5159bb0d570f73afad76f54f07cf715cdbb5c68
Pat Blau <that_email_here>
2026-03-07 18:33:49 -0600

    Change Entry constructor from private to public

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -145,7 +145,7 @@ public class Entry
     private string _date;
     private string _prompt;
     private string _response;
-    private Entry(string date, string prompt, string response)
+    public Entry(string date, string prompt, string response)
     {
         _date = date;
         _prompt = prompt;
